d34af09c70289aee261ae8437f6dacbde6ac4eaa
DoloresPark <dolorespark@e-vertise.com>
2015-10-14 23:40:00 -0400

    M: keymaster - update to reflect legacy status

--- keymaster/Android.mk
+++ keymaster/Android.mk
@@ -12,38 +12,25 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-ifeq ($(TARGET_ARCH),arm)
 ifneq (,$(filter m470, $(TARGET_DEVICE)))
 
-# This is a nasty hack. keystore.m470 is Open Source, but it
-# links against a non-Open library, so we can only build it
-# when that library is present.
+# Keystore.m470 is Open Source, but it links against a non-Open library,
+# Print a warning and skip this module if the library is not present in the
+# vendor directory or BOARD_HAS_TF_CRYPTO_SST has not been set
+BOARD_HAS_TF_CRYPTO_SST ?= $(if $(wildcard vendor/*/$(TARGET_DEVICE)/*/libtf_crypto_sst.so),true)
+
 ifeq ($(BOARD_HAS_TF_CRYPTO_SST),true)
+	LOCAL_PATH := $(call my-dir)
+	include $(CLEAR_VARS)
+	LOCAL_MODULE := keystore.m470
+	LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
+	LOCAL_SRC_FILES := keymaster_m470.cpp
+	LOCAL_CFLAGS := -Werror
+	LOCAL_SHARED_LIBRARIES := libcutils liblog libcrypto libtf_crypto_sst
+	LOCAL_MODULE_TAGS := optional
+	include $(BUILD_SHARED_LIBRARY)
+else
+$(warning WARNING : Skipping keystore.m470 - Dependency Not Found libtf_crypto_sst )
 
-LOCAL_PATH := $(call my-dir)
-
-include $(CLEAR_VARS)
-
-LOCAL_MODULE := keystore.m470
-
-LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
-
-LOCAL_SRC_FILES := \
-	keymaster_m470.cpp
-
-LOCAL_C_INCLUDES := \
-	libcore/include \
-	external/openssl/include \
-	$(LOCAL_PATH)/../security/tf_sdk/include
-
-LOCAL_CFLAGS := -fvisibility=hidden -Wall -Werror
-
-LOCAL_SHARED_LIBRARIES := libcutils liblog libcrypto libtf_crypto_sst
-
-LOCAL_MODULE_TAGS := optional
-
-include $(BUILD_SHARED_LIBRARY)
-
-endif
 endif
 endif
