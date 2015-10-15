d7bb889abf752982930e535bdf0c6e5c060904ae
DoloresPark <dolorespark@e-vertise.com>
2015-10-14 23:37:00 -0400

    M: remove incompatible/unnecessary codecs

--- device.mk
+++ device.mk
@@ -102,7 +102,6 @@ PRODUCT_COPY_FILES += \
 ### media codec configs
 PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
-    frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
     device/hisense/m470/prebuilt/etc/media_codecs.xml:system/etc/media_codecs.xml
 
